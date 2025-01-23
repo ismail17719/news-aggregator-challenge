0b3b6149cb03d99e2a3d5748518cc587b563d168
M. Ismail <ismail.m.17719@gmail.com>
2025-01-23 13:25:50 +0500

    feat: The New York Times news aggregation added

--- app/Services/Aggregations/NytNews.php
+++ app/Services/Aggregations/NytNews.php
@@ -2,18 +2,23 @@
 
 namespace App\Services\Aggregations;
 
+use App\Actions\News\StoreArticleAction;
+use App\Dtos\News\ArticleDto;
+use App\Enums\News\ArticleSource;
 use App\Traits\ConsumeExternalService;
+use Illuminate\Support\Collection;
 
 class NytNews implements Aggregator
 {
     use ConsumeExternalService;
+    protected Collection $news;
 
     /**
      * Create a new class instance.
      */
     public function __construct()
     {
-        //
+        $this->news = collect();
     }
 
     /**
@@ -22,7 +27,23 @@ class NytNews implements Aggregator
      */
     public function fetch(): void
     {
-        //
+        $results = json_decode($this->request(
+            config('services.news.nyt.endpoint'),
+            [
+                'q' => 'a',
+                'begin_date' => now()->format('Ymd'),
+                'end_date' => now()->format('Ymd'),
+                'api-key' => config('services.news.nyt.key')
+            ]
+        ));
+        foreach ($results->articles as $news) {
+            $this->news->push( new ArticleDto(
+                $news->title,
+                $news->url,
+                ArticleSource::NYT,
+                count($news->multimedia[0]) > 0 ? $news->multimedia[0]->url : null
+            ));
+        }
     }
 
     /**
@@ -31,6 +52,8 @@ class NytNews implements Aggregator
      */
     public function store(): void
     {
-        //
+        foreach ($this->news as $article) {
+            app(StoreArticleAction::class)->__invoke($article);
+        }
     }
 }
