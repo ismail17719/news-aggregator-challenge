7d5518f1739a20654b3ef87a92c5da6333c394f7
M. Ismail <ismail.m.17719@gmail.com>
2025-01-23 08:24:23 +0500

    External service consumer trait added

---
+++ app/Traits/ConsumeExternalService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Http;
+
+trait ConsumeExternalService
+{
+    /**
+     * Send a GET request to any service
+     */
+    public function request(string $requestUrl, array $params = [], array $headers = []): string
+    {
+        $response = Http::retry(3, 1000)
+            ->withQueryParameters($params)
+            ->withHeaders($headers)
+            ->asJson()
+            ->get($requestUrl);
+        if($response->failed()){
+            throw new \Exception($response->body());
+        }
+
+        return $response->body();
+    }
+}
